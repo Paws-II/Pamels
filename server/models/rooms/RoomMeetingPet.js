7d177184b2e52df4f2bfef2437ba0eaacee8207c
Ayush <ayushjha21157@gmail.com>
2025-12-21 15:11:20 +0530

    Add: application work under progress

---
+++ server/models/rooms/RoomMeetingPet.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const roomMeetingPetSchema = new mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "OwnerLogin",
+      required: true,
+      index: true,
+    },
+    shelterId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ShelterLogin",
+      required: true,
+      index: true,
+    },
+    petId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "PetProfile",
+      required: true,
+      index: true,
+    },
+    applicationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AdoptionApplication",
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+roomMeetingPetSchema.index({ applicationId: 1 }, { unique: true });
+
+export default mongoose.model("RoomMeetingPet", roomMeetingPetSchema);
